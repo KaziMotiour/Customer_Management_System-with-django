3adfddf83723acf9ba9c387f271398835bee04fd
motiour <kmatiour30@gmail.com>
2020-04-19 23:06:34 +0600

    Data base tabel added

--- customer_management_system/acc/models.py
+++ customer_management_system/acc/models.py
@@ -2,16 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+
+
+
 class Customer(models.Model):
     name=models.CharField(max_length=250)
     email=models.CharField(max_length=250)
     phone=models.CharField(max_length=250)
     profile_pic=models.ImageField(upload_to='profile_pic', null=True, blank=True)
+ 
     create_date=models.DateTimeField(auto_now_add=True, null=True)
 
 
     def __str__(self):
         return str(self.name)
+
+
+class Tag(models.Model):
+    tag_name=models.CharField(max_length=250, null=True, blank=True)
 
 class Product(models.Model):
     CATAGORY=(
@@ -23,6 +31,7 @@ class Product(models.Model):
     category=models.CharField(max_length=250, null=True, blank=True, choices=CATAGORY)
     description=models.CharField(max_length=500, null=True, blank=True)
     product_pic=models.ImageField(upload_to='profile_pic', null=True, blank=True)
+    tag_name=models.ManyToManyField(Tag)
     create_date=models.DateTimeField(auto_now_add=True, null=True)
 
 
@@ -35,6 +44,11 @@ class Order(models.Model):
         ('Out of delivery','Out of delivery'),
         ('Delivered','Delivered')
     )
+    coustomer=models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
+    product=models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
     date_created=models.DateTimeField(auto_now_add=True, null=True, blank=True)
     status=models.CharField(max_length=250, null=True, choices=STATUS)
+
+    def __str__(self):
+        return str(self.status)
     
